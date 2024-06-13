616f9455a8637145c3ec3d59f0c96acf64902df8
unknown <nguyenthangloi19052004@gmail.com>
2024-06-13 13:51:05 +0700

    architecture design more and file cteated

---
+++ src/main/java/com/loinguyen1905/ecommerce/orderItems/OrderItemController.java
@@ -0,0 +1,36 @@
+package com.loinguyen1905.ecommerce.users;
+
+import org.apache.coyote.BadRequestException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.loinguyen1905.ecommerce.models.ApiResponse;
+import com.loinguyen1905.ecommerce.models.ResponseBuilder;
+import com.loinguyen1905.ecommerce.users.dto.UserDto;
+
+@RestController
+@RequestMapping("/api/v1/users/")
+@SuppressWarnings("rawtypes")
+// @SecurityRequirement(name = "E-Commerce Application")
+public class UserController {
+
+    @Autowired
+	private UserService userService;
+
+	
+    @Autowired
+    private ResponseBuilder responseBuilder;
+    
+	@PostMapping("")
+	public ResponseEntity<ApiResponse> register(@RequestBody UserDto userDto) throws BadRequestException{
+		userDto = this.userService.registerUser(userDto);
+		return responseBuilder.buildResponse(HttpStatus.CREATED.value(),"Create a new user", userDto);
+	}
+
+}
